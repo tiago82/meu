b300eb495f5af9329145410ab3dc2a35bdbf6ff4
tiago8204 <tiago8204@gmail.com>
2019-11-29 12:34:35 -0300

    Add files via upload

--- instalar-chrome.sh
+++ instalar-chrome.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-## criado por tiago8204 29/11/2019 ##
+## Criado por tiago8204 29/11/2019 ##
 cd /tmp
 
 
-## variaveis (alterar caso o link mude, ou para adptar outro programa no script ###
+## Variaveis (alterar caso o link mude, ou para adptar outro programa no script) ##
 URL_GOOGLE_CHROME="https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb"
 DEB_CHROME="google-chrome-stable_current_amd64.deb"
 
 
-## remove vesoes anteriores  ("rm" para excluir) ("-f" para ingnorar caso nao exista )## 
+## Remove vesoes anteriores  ("rm" para excluir) ("-f" para ingnorar caso nao exista) ## 
 rm -f "$DEB_CHROME"
 
 
@@ -18,7 +18,7 @@ echo -------------------------------------------
 echo
 
 
-## baixa arquivos de instalação  ##
+## Baixa arquivos de instalação ##
 sleep 3
 wget "$URL_GOOGLE_CHROME"
 clear
@@ -29,7 +29,7 @@ echo
 
 
 
-## realiza a instação com dpkg ##
+## Realiza a instalação com dpkg ##
 sudo dpkg -i "$DEB_CHROME"
 rm "$DEB_CHROME"
 echo 
