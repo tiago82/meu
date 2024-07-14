15a533bb030a7682e3677be01b4617d1f06a20d9
Tiago <tiago8204@gmail.com>
2024-07-14 09:53:37 -0300

    Update meu-uso.sh

--- meu-uso.sh
+++ meu-uso.sh
@@ -10,4 +10,3 @@ cd /tmp/baixa
 wget -c https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 wget -c https://update.code.visualstudio.com/latest/linux-deb-x64/stable -O vscode.deb
 sudo dpkg
-
